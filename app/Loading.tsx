f4ca1f98391a7f2880400c29b431347aa1f02cce
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-12-28 14:58:43 +0200

    add loading component

---
+++ app/Loading.tsx
@@ -0,0 +1,5 @@
+import  CustomLoading from "@/components/custom/Loading";
+
+export default function Loading() {
+  return <CustomLoading/>
+}
---
+++ components/custom/Loading.tsx
@@ -0,0 +1,17 @@
+import Loader from "react-spinners/PuffLoader";
+
+function Loading() {
+  return (
+    <div className="flex w-full h-full items-center justify-center">
+      <Loader
+        color="hsl(0 0% 20%)"
+        loading={true}
+        size={30}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
+}
+
+export default Loading
