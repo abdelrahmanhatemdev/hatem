84192665da89eec3a184b3dd7a14c2510c337177
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-12-30 12:14:47 +0200

    add hero swiper animation

--- components/modules/home/Hero.tsx
+++ components/modules/home/Hero.tsx
@@ -1,18 +1,29 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
-
 import "swiper/css";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { cn, cubicBezier } from "@/lib/utils";
+import { motion } from "framer-motion";
 
 const Hero = () => {
-  const [sliderIndex, setSliderIndex] = useState<number | null>(null);
+  const [activeSlide, setActiveSlide] = useState<number | null>(null);
   const [progress, setProgress] = useState<number>(0);
   const [isAutoplaying, setIsAutoplaying] = useState<boolean>(false);
 
-  const autoplayDelay = 5000;
+  const autoplayDelay = 8000;
+
+  const slideVariants = {
+    initial: { opacity: 0, x: 200 },
+    active: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 200 },
+  };
+
+  const slideTransition = { duration: 1, ease: [0.4, 0, 0.2, 1] };
+
+  const slideParagraphDelay = 0.4;
+  const slideSpanDelay = 0.2;
 
   useEffect(() => {
     const easeInOut = cubicBezier(0.9, 0, 0.2, 1);
@@ -25,27 +36,22 @@ const Hero = () => {
       const elapsed = now - startTime;
 
       const linearProgress = elapsed / duration;
-      
 
       if (linearProgress >= 1.038) {
-          startTime = now;
-         
+        startTime = now;
       }
 
       const easedProgress = easeInOut(Math.min(linearProgress, 1));
       setProgress(easedProgress * 100);
-      
 
-      requestAnimationFrame(animateProgress); 
-      if (linearProgress < 1) {
-      }
+      requestAnimationFrame(animateProgress);
     };
 
     if (isAutoplaying) {
-      requestAnimationFrame(animateProgress); 
+      requestAnimationFrame(animateProgress);
     }
 
-    return () => cancelAnimationFrame(lastTime); 
+    return () => cancelAnimationFrame(lastTime);
   }, [isAutoplaying]);
 
   return (
@@ -67,11 +73,11 @@ const Hero = () => {
         onSlideChange={(swiper) => {
           const newIndex = swiper.realIndex + 1;
 
-          if (newIndex === sliderIndex) {
+          if (newIndex === activeSlide) {
             return;
           }
 
-          setSliderIndex(newIndex);
+          setActiveSlide(newIndex);
           setIsAutoplaying(true);
         }}
         onAutoplayStart={() => setIsAutoplaying(true)}
@@ -80,83 +86,213 @@ const Hero = () => {
           setProgress(0);
         }}
       >
-        <SwiperSlide>
-          <div className="h-full flex flex-col justify-center ">
-            <p className="text-[4vw] leading-[6vw] flex items-center gap-[1vw]">
-              <span>
-                <span className="font-extralight">I</span>
-                <span className="font-extralight">'m </span>
-              </span>
-              <span className="font-thin">a</span>
-              <span className="w-[10vw] h-[5vw] relative inline-block">
-                <Image src="/assets/images/nextjs.png" fill alt="Next.js" />
-              </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                Developer
-              </strong>
-            </p>
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-light">Specialized </span>
-              <span className="font-thin">in </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                Frontend Development
-              </strong>
-            </p>
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-extralight">Based </span>
-              <span className="font-thin">in </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                Cairo
-              </strong>
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="h-full flex flex-col justify-center">
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-thin">Focused on </span>
-              <span className="font-extralight">building </span>
-            </p>
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-extralight">blazing-fast </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                performant, scalable,
-              </strong>
-            </p>
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-thin">and </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                seamlessly integrated
-              </strong>
-              <span className="font-extralight"> web apps </span>
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="h-full flex flex-col justify-center">
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-thin"> With </span>
-              <span className="font-light">Code Speaks </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                Performance
-              </strong>
-            </p>
-            <p className="text-[4vw] leading-[6vw]">
-              <span className="font-thing">I </span>
-              <span className="font-extralight">value </span>
-              <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
-                Quality
-              </strong>
-              <span className="font-light"> over </span>
-              <span className="font-thin opacity-90 text-[3.5rem] text-neutral-300">
-                Quantity
-              </span>
-            </p>
-          </div>
-        </SwiperSlide>
+        {[1, 2, 3].map((slide, index) => (
+          <SwiperSlide key={index}>
+            {slide === 1 && (
+              <div className="h-full flex flex-col justify-center">
+                <p className="text-[4vw] leading-[6vw] flex items-center gap-[1vw]">
+                  <motion.span
+                    variants={slideVariants}
+                    initial={`initial`}
+                    animate={activeSlide === index + 1 ? `active` : `initial`}
+                    exit={`exit`}
+                    transition={{ ...slideTransition, delay: 0 }}
+                  >
+                    <span>
+                      <span className="font-extralight">I</span>
+                      <span className="font-extralight">'m </span>
+                    </span>
+                    <span className="font-thin">a</span>
+                  </motion.span>
+                  <motion.span
+                    variants={slideVariants}
+                    initial={`initial`}
+                    animate={activeSlide === index + 1 ? `active` : `initial`}
+                    exit={`exit`}
+                    transition={{
+                      ...slideTransition,
+                      delay: 1* slideSpanDelay,
+                    }}
+                    className="w-[10vw] h-[5vw] relative inline-block"
+                  >
+                    <span >
+                      <Image
+                        src="/assets/images/nextjs.png"
+                        fill
+                        alt="Next.js"
+                      />
+                    </span>
+                  </motion.span>
+                  <motion.span
+                    variants={slideVariants}
+                    initial={`initial`}
+                    animate={activeSlide === index + 1 ? `active` : `initial`}
+                    exit={`exit`}
+                    transition={{
+                      ...slideTransition,
+                      delay: 2* slideSpanDelay,
+                    }}
+                  >
+                    <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                      Developer
+                    </strong>
+                  </motion.span>
+                </p>
+
+                <motion.span
+                  variants={slideVariants}
+                  initial={`initial`}
+                  animate={activeSlide === index + 1 ? `active` : `initial`}
+                  exit={`exit`}
+                  transition={{
+                    ...slideTransition,
+                    delay: 1 * slideParagraphDelay,
+                  }}
+                >
+                  <p className="text-[4vw] leading-[6vw]">
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: (1 * slideParagraphDelay) + slideSpanDelay,
+                      }}
+                    >
+                      <span className="font-light">Specialized </span>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: (1 * slideParagraphDelay) + (2 * slideSpanDelay),
+                      }}
+                    >
+                      <span className="font-thin">in </span>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: (1 * slideParagraphDelay) + (3 * slideSpanDelay),
+                      }}
+                    >
+                      <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                        Frontend 
+                      </strong>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: (1 * slideParagraphDelay) + (4 * slideSpanDelay),
+                      }}
+                    >
+                      <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                      {" "}Development
+                      </strong>
+                    </motion.span>
+                  </p>
+                </motion.span>
+                <motion.span
+                  variants={slideVariants}
+                  initial={`initial`}
+                  animate={activeSlide === index + 1 ? `active` : `initial`}
+                  exit={`exit`}
+                  transition={{ ...slideTransition, delay: (2 * slideParagraphDelay) }}
+                >
+                  <p className="text-[4vw] leading-[6vw]">
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{ ...slideTransition, delay: (2 * slideParagraphDelay) + (1 * slideSpanDelay) }}
+                    >
+                      <span className="font-extralight">Based </span>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{ ...slideTransition, delay: (2 * slideParagraphDelay) + (2 * slideSpanDelay) }}
+                    >
+                      <span className="font-thin">in </span>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{ ...slideTransition, delay:(2 * slideParagraphDelay) + (3 * slideSpanDelay) }}
+                    >
+                      <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                        Cairo
+                      </strong>
+                    </motion.span>
+                  </p>
+                </motion.span>
+              </div>
+            )}
+            {slide === 2 && (
+              <div className="h-full flex flex-col justify-center">
+                <p className="text-[4vw] leading-[6vw]">
+                  <span className="font-thin">Focused on </span>
+                  <span className="font-extralight">building </span>
+                </p>
+                <p className="text-[4vw] leading-[6vw]">
+                  <span className="font-extralight">blazing-fast </span>
+                  <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                    performant, scalable,
+                  </strong>
+                </p>
+                <p className="text-[4vw] leading-[6vw]">
+                  <span className="font-thin">and </span>
+                  <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                    seamlessly integrated
+                  </strong>
+                  <span className="font-extralight"> web apps </span>
+                </p>
+              </div>
+            )}
+            {slide === 3 && (
+              <div className="h-full flex flex-col justify-center">
+                <p className="text-[4vw] leading-[6vw]">
+                  <span className="font-thin"> With </span>
+                  <span className="font-light">Code Speaks </span>
+                  <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                    Performance
+                  </strong>
+                </p>
+                <p className="text-[4vw] leading-[6vw]">
+                  <span className="font-thing">I </span>
+                  <span className="font-extralight">value </span>
+                  <strong className="font-extrabold tracking-wider text-[4.5vw] scale-y-105">
+                    Quality
+                  </strong>
+                  <span className="font-light"> over </span>
+                  <span className="font-thin opacity-90 text-[3.5rem] text-neutral-300">
+                    Quantity
+                  </span>
+                </p>
+              </div>
+            )}
+          </SwiperSlide>
+        ))}
         <div className="absolute [transition:opacity_0.3s] z-10 bottom-[7vh] top-[var(--swiper-pagination-top, auto)] left-[0] flex gap-[6vw]">
           {[1, 2, 3].map((slider) => {
-            const isActive = slider === sliderIndex;
+            const isActive = slider === activeSlide;
             return (
               <div className="flex items-center gap-[2vw]" key={slider}>
                 <span
@@ -171,7 +307,10 @@ const Hero = () => {
                   <div className="h-[0.01rem] w-[65vw] bg-muted">
                     <div
                       className="h-full bg-foreground"
-                      style={{ width: `${progress}%`, transitionTimingFunction: "ease-in-out" }}
+                      style={{
+                        width: `${progress}%`,
+                        transitionTimingFunction: "ease-in-out",
+                      }}
                     ></div>
                   </div>
                 )}
@@ -183,4 +322,4 @@ const Hero = () => {
     </section>
   );
 };
-export default Hero;
+export default memo(Hero);
