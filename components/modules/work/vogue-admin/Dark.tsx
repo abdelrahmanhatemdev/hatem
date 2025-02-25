533a4c0ee9982471cb3a7955fc609e04b86919b3
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-02-25 20:13:28 +0200

    add project dark component

--- components/modules/work/vogue-admin/Dark.tsx
+++ components/modules/work/vogue-admin/Dark.tsx
@@ -4,39 +4,59 @@ import { memo } from "react";
 const Dark = () => {
   return (
     <section className="px-6">
-      <div className="flex flex-col gap-10 justify-center items-center bg-neutral-950/50 py-32 ">
-        <div className="relative">
-          <div className="flex flex-col lg:flex-row gap-16 justify-center items-center">
-            <div>
+      <div className=" bg-neutral-950/50 pt-32 overflow-hidden ">
+        <div className="flex flex-col gap-32 justify-center items-center">
+          <div className="grid grid-cols-1 md:grid-cols-3 max-w-[900px]">
+            <div className="px-5">
               <Image
-                src="/assets/media/work/vogue-admin/dashboard.webp"
+                src="/assets/media/work/vogue-admin/dashboard-sm.webp"
                 alt="Dark Theme 1"
-                className="rounded-2xl border-[10px] border-neutral-950/50"
+                className="rounded-2xl border-[5px] border-neutral-950/50"
                 width={1000}
                 height={620}
               />
             </div>
-            <div>
+            <div className="px-5 pt-16">
               <Image
-                src="/assets/media/work/vogue-admin/dashboard.webp"
-                alt="Dark Theme 2"
-                className="rounded-2xl border-[10px] border-neutral-950/50"
+                src="/assets/media/work/vogue-admin/dashboard-sm.webp"
+                alt="Dark Theme 1"
+                className="rounded-2xl border-[5px] border-neutral-950/50"
                 width={1000}
                 height={620}
               />
             </div>
-            <div>
+            <div className="px-5 pt-32">
               <Image
-                src="/assets/media/work/vogue-admin/dashboard.webp"
-                alt="Dark Theme 3"
-                className="rounded-2xl border-[10px] border-neutral-950/50"
+                src="/assets/media/work/vogue-admin/dashboard-sm.webp"
+                alt="Dark Theme 1"
+                className="rounded-2xl border-[5px] border-neutral-950/50"
                 width={1000}
                 height={620}
               />
             </div>
           </div>
-          <div>Content</div>
+          <div className="flex flex-col gap-10 sticky top-0 px-32 w-full">
+            <h2 className="font-ogg text-6xl">
+              <p className="leading-20">Breathtaking Experience</p>
+              <p className="leading-20"> In Light and Dark</p>
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 max-w-[800px]">
+              <p className="text-lg font-medium text-neutral-400">
+                Dark mode improves visual comfort by minimizing glare and
+                reducing eye fatigue, making extended screen use easier on the
+                eyes. It adapts to different lighting conditions, ensuring a
+                smooth and comfortable experience, especially in dim
+                environments.
+              </p>
+              <p className="text-lg font-medium text-neutral-400 md:px-5">
+                Beyond aesthetics, dark mode boosts battery life on OLED and
+                AMOLED screens by using less power, allowing for longer device
+                usage.
+              </p>
+            </div>
+          </div>
         </div>
+        <div className="font-ogg text-[40vw] h-[36vw] flex justify-center">Dark</div>
       </div>
     </section>
   );
