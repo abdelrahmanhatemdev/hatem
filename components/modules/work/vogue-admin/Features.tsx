9fbce11d113a8b8d9ee93ed340bf08b65e20686b
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-02-26 09:17:15 +0200

    add transition to work feature list

--- components/modules/work/vogue-admin/Features.tsx
+++ components/modules/work/vogue-admin/Features.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { memo } from "react";
 
 const featues = [
@@ -81,16 +82,30 @@ const Features = () => {
       <div className="bg-neutral-950/50 rounded-[7rem] p-[5rem] flex flex-col gap-10">
         {featues.map((featue, index) => (
           <div
-            className="flex justify-between items-end border-b-[5px] border-neutral-50 pb-5"
+            className={cn(
+              "flex justify-between items-end group cursor-pointer",
+              index !== featues.length - 1
+                ? "border-b-[5px] border-neutral-50 pb-5"
+                : ""
+            )}
             key={index}
           >
             <h2 className="text-[5vw] font-ogg leading-24">{featue.title}</h2>
             <ul className="flex flex-col gap-3 w-[280px]">
-              {featue.list.map((item, i) => (
-                <li className="font-satoshi font-medium" key={i}>
-                  {item}
-                </li>
-              ))}
+              {featue.list.map((item, i) => {
+                return (
+                  <li
+                    className="font-satoshi font-medium opacity-0 group-hover:opacity-100 transition-all duration-500"
+                    style={{
+                      transitionDelay:
+                        i !== 0 ? `${i * 80}ms` : "",
+                    }}
+                    key={i}
+                  >
+                    {item}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         ))}
