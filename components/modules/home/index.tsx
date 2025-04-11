72414c683e97684b35929e36c67deed92c342ed5
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-04-11 13:47:31 +0200

    improve performance

--- components/modules/home/index.tsx
+++ components/modules/home/index.tsx
@@ -39,7 +39,7 @@ const Home = () => {
   return (
     <>
       {/* <Hero /> */}
-      {/* <About /> */}
+      <About />
       <SelectedWorks/>
       {/* <Expertise /> */}
       {/* <WorkProcess /> */}
