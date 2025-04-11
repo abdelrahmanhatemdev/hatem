9cbce24af8c447af2396dd730ab72d773d4102cf
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-04-11 03:28:08 +0200

    deactive major sections to test performance

--- components/custom/Logo.tsx
+++ components/custom/Logo.tsx
@@ -6,11 +6,13 @@ const Logo = ({ className = "" }: { className?: string }) => {
     <div className={cn("font-satosh font-black flex gap-2 lg:text-2xl", className)}>
       <div className="relative group perspective-dramatic h-8 min-w-40">
         <div>
-          <h2 className="opacity-50 absolute inset-0 flex transition-all group-hover:rotate-x-90 duration-300 ease-in">
-            <span>Abdelrahman</span>
+          <h2 className="opacity-60 absolute inset-0 flex transition-all group-hover:rotate-x-90 duration-300 ease-in">
+            <span>A</span>
+            <span className="lowercase">bdelrahman</span>
           </h2>
           <h2 className="absolute inset-0 flex transition-all -rotate-x-90 translate-y-6 group-hover:rotate-x-0 group-hover:translate-y-0 group-hover:opacity-100 duration-300 ease-in">
-            <span>Frontend</span>
+            <span>F</span>
+            <span className="lowercase">rontend</span>
           </h2>
         </div>
         <div>
@@ -18,7 +20,7 @@ const Logo = ({ className = "" }: { className?: string }) => {
             <span>H</span>
             <span className="lowercase">atem</span>
           </h2>
-          <h2 className="opacity-50 absolute itop-0 left-[7rem] flex transition-all rotate-x-90 -translate-y-6 group-hover:rotate-x-0 group-hover:translate-y-0 group-hover:opacity-50 duration-300 ease-in">
+          <h2 className="opacity-60 absolute itop-0 left-[7rem] flex transition-all rotate-x-90 -translate-y-6 group-hover:rotate-x-0 group-hover:translate-y-0 group-hover:opacity-50 duration-300 ease-in">
             <span>D</span>
             <span className="lowercase">eveloper</span>
           </h2>
