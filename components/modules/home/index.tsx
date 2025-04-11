f87d8d2658036a6fd2a2552281dca767692edc9b
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-04-11 03:03:04 +0200

    deactive major sections to test performance

--- components/modules/home/index.tsx
+++ components/modules/home/index.tsx
@@ -1,6 +1,6 @@
 "use client"
-import dynamic from "next/dynamic";
-import Loading from "@/components/custom/Loading";
+// import dynamic from "next/dynamic";
+// import Loading from "@/components/custom/Loading";
 
 // const Hero = dynamic(() => import("@/components/modules/home/Hero"), {
 //   loading: Loading,
