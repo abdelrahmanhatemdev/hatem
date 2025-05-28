6e014627fef85ab5ff5249bf137869b84bbc3434
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-05-28 14:56:57 +0300

    add custom animate function and component

---
+++ components/custom/animation/Animate.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { motion, type Variants } from "framer-motion";
+import type { ReactNode } from "react";
+
+type MotionElementTag = "div" | "section" | "article" | "header" | "footer" | "main" | "nav" | "p" | "span" | "a" | "button" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+interface AnimateOptions {
+  children?: ReactNode;
+  variants: Variants;
+  element?: MotionElementTag;
+  className?: string;
+  viewOnce?: boolean;
+}
+
+const Animate = ({
+  children,
+  variants,
+  element = "div",
+  className,
+  viewOnce = false
+}: AnimateOptions) => {
+  const MotionTag = motion[element];
+
+  return (
+    <MotionTag
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: viewOnce }}
+      variants={variants}
+      className={className}
+    >
+      {children}
+    </MotionTag>
+  );
+};
+
+export default Animate;
