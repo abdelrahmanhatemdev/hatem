88d695a5edf6219a033c975bb07eebacfa1d15b0
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-05-30 10:34:44 +0300

    split code components swipers

--- components/modules/home/WorkProcess.tsx
+++ components/modules/home/WorkProcess.tsx
@@ -1,13 +1,8 @@
-"use client";
 import { memo } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import { animate } from "@/lib/animation/animate";
 import dynamic from "next/dynamic";
-import { workProcess } from "@/data/workProcess";
-const Animate = dynamic(() => import("@/components/custom/animation/Animate"));
-
-const fade = animate({ type: "fade" });
+const WorkProcessSwiper = dynamic(
+  () => import("@/components/modules/home/workProcess/WorkProcessSwiper")
+);
 
 const WorkProcess = () => {
   return (
@@ -16,74 +11,7 @@ const WorkProcess = () => {
         <h2 className="text-4xl md:text-6xl text-center p-4">
           My Work Process
         </h2>
-        <div className="grid gap-5 lg:gap-10 ">
-          {workProcess.map((item, i) => (
-            <Animate
-              viewOnce={true}
-              variants={{
-                ...fade,
-                visible: {
-                  ...fade.visible,
-                  transition: {
-                    ...fade.visible.transition,
-                    delay: 0.05 * (i + 1),
-                  },
-                },
-              }}
-              key={`${i}`}
-            >
-              <div className="grid lg:grid-cols-[calc(35%-1.25rem)_calc(65%-1.25rem)] gap-5 lg:gap-10 border-b border-b-neutral-800 pb-20 md:pb-12">
-                <div className="flex flex-col gap-3 pt-8 lg:pt-0">
-                  <h2 className="text-3xl bg-gradient-to-b from-sky-600 to-sky-400 bg-clip-text text-transparent">
-                    0{++i}.
-                  </h2>
-                  <p className="text-3xl">{item.title}</p>
-                </div>
-                <div className="grid gap-10">
-                  <p className="text-neutral-500 text-lg font-semibold">
-                    {item.details}
-                  </p>
-
-                  <Swiper
-                    modules={[Autoplay]}
-                    className="w-full relative"
-                    breakpoints={{
-                      320: {
-                        slidesPerView: 3,
-                        speed: 8000,
-                        spaceBetween: 10,
-                      },
-                      768: {
-                        slidesPerView: 4,
-                        speed: 15000,
-                        spaceBetween: 20,
-                      },
-                    }}
-                    loop={true}
-                    autoplay={{
-                      delay: 0,
-                      disableOnInteraction: false,
-                      reverseDirection: i % 2 === 0 ? true : false,
-                    }}
-                    speed={15000}
-                  >
-                    <div className="h-full w-40 bg-gradient-to-r from-black to-transparent absolute inset-0 z-10"></div>
-                    {item.keywords.map((item) => (
-                      <SwiperSlide key={item}>
-                        <div className="flex justify-center items-center">
-                          <div className="bg-gradient-to-bl from-neutral-950 to-[#202020] py-2 px-4 md:py-4 md:px-8 rounded-full flex justify-center items-center text-sm border border-neutral-900">
-                            {item}
-                          </div>
-                        </div>
-                      </SwiperSlide>
-                    ))}
-                    <div className="h-full w-40 bg-gradient-to-l from-black to-transparent absolute top-0 end-0 z-10"></div>
-                  </Swiper>
-                </div>
-              </div>
-            </Animate>
-          ))}
-        </div>
+        <WorkProcessSwiper />
       </div>
     </section>
   );
---
+++ components/modules/home/hero/HeroSwiper.tsx
@@ -0,0 +1,277 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import { memo, useEffect, useState } from "react";
+import { cn, cubicBezier } from "@/lib/utils";
+import { motion } from "framer-motion";
+import { slideTransition, slideVariants } from "@/lib/animation/slider";
+
+function HeroSwiper() {
+    const [activeSlide, setActiveSlide] = useState<number | null>(1);
+  const [progress, setProgress] = useState<number>(0);
+  const [isAutoplaying, setIsAutoplaying] = useState<boolean>(false);
+  const [isProgressing, setIsProgressing] = useState<boolean>(false);
+
+  const autoplayDelay = 8000;
+
+  const slideParagraphDelay = 0.4;
+  const slideSpanDelay = 0.2;
+
+  useEffect(() => {
+    const easeInOut = cubicBezier(0.9, 0, 0.2, 1);
+    const duration = autoplayDelay;
+    let startTime: number | null = null;
+    let animationFrameId: number;
+
+    const animateProgress = (now: number) => {
+      if (startTime === null) startTime = now;
+      const elapsed = now - startTime;
+
+      const linearProgress = elapsed / duration;
+
+      if (linearProgress >= 1) {
+        startTime = now;
+        setIsProgressing(false);
+        setIsAutoplaying(false);
+        setProgress(100);
+        return;
+      }
+
+      const easedProgress = easeInOut(Math.min(linearProgress, 1));
+
+      setProgress(easedProgress * 100);
+
+      animationFrameId = requestAnimationFrame(animateProgress);
+    };
+
+    if (isAutoplaying && isProgressing) {
+      animationFrameId = requestAnimationFrame(animateProgress);
+    }
+
+    return () => {
+      cancelAnimationFrame(animationFrameId); // Cancel the animation frame.
+    };
+  }, [isAutoplaying, isProgressing, autoplayDelay, activeSlide]);
+  return (
+    <Swiper
+        modules={[Autoplay]}
+        pagination={{
+          clickable: true,
+          renderBullet: (index, className) => {
+            return `<span class="${className}">0${index + 1}.</span>`;
+          },
+        }}
+        autoplay={{
+          delay: autoplayDelay,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className="h-full lg:w-[85%] mx-auto hero-swiper relative"
+        onSlideChange={(swiper) => {
+          const newIndex = swiper.realIndex + 1;
+
+          if (newIndex === activeSlide) {
+            return;
+          }
+          setProgress(0);
+          setActiveSlide(newIndex);
+          setIsAutoplaying(true);
+          setIsProgressing(true);
+        }}
+        onAutoplayStart={() => {
+          setIsAutoplaying(true);
+          setIsProgressing(true);
+        }}
+        onAutoplayStop={() => {
+          setIsAutoplaying(false);
+          setIsProgressing(false);
+          setProgress(0);
+        }}
+        spaceBetween={20}
+      >
+        {[1, 2, 3].map((slide, index) => (
+          <SwiperSlide key={index}>
+            {slide === 1 && (
+              <div className="h-full flex flex-col justify-center">
+                <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw] flex items-center gap-[2vw] md:gap-[1vw] flex-wrap">
+                  <motion.span
+                    variants={slideVariants}
+                    initial={`initial`}
+                    animate={activeSlide === index + 1 ? `active` : `initial`}
+                    exit={`exit`}
+                    transition={{ ...slideTransition, delay: 0 }}
+                    className="flex gap-[2vw]"
+                  >
+                    <span>
+                      <span className="font-extralight">I</span>
+                      <span className="font-extralight">&apos;m </span>
+                    </span>
+                    <span className="font-thin">a</span>
+                  </motion.span>
+
+                  <motion.span
+                    variants={slideVariants}
+                    initial={`initial`}
+                    animate={activeSlide === index + 1 ? `active` : `initial`}
+                    exit={`exit`}
+                    transition={{
+                      ...slideTransition,
+                      delay: 1 * slideSpanDelay,
+                    }}
+                    className="font-extrabold md:tracking-wider text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] scale-y-105"
+                  >
+                    <span>Frontend</span>
+                  </motion.span>
+
+                  <motion.span
+                    variants={slideVariants}
+                    initial={`initial`}
+                    animate={activeSlide === index + 1 ? `active` : `initial`}
+                    exit={`exit`}
+                    transition={{
+                      ...slideTransition,
+                      delay: 2 * slideSpanDelay,
+                    }}
+                  >
+                    <strong className="inline-block font-extralight">
+                      Developer
+                    </strong>
+                  </motion.span>
+                </p>
+
+                <motion.span
+                  variants={slideVariants}
+                  initial={`initial`}
+                  animate={activeSlide === index + 1 ? `active` : `initial`}
+                  exit={`exit`}
+                  transition={{
+                    ...slideTransition,
+                    delay: 1 * slideParagraphDelay,
+                  }}
+                >
+                  <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw] flex gap-2 lg:gap-4 items-center">
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: 1 * slideParagraphDelay + slideSpanDelay,
+                      }}
+                    >
+                      <span className="font-light">Specialized </span>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: 1 * slideParagraphDelay + 2 * slideSpanDelay,
+                      }}
+                    >
+                      <span className="font-thin">in </span>
+                    </motion.span>
+                    <motion.span
+                      variants={slideVariants}
+                      initial={`initial`}
+                      animate={activeSlide === index + 1 ? `active` : `initial`}
+                      exit={`exit`}
+                      transition={{
+                        ...slideTransition,
+                        delay: 1 * slideParagraphDelay + 3 * slideSpanDelay,
+                      }}
+                      className="inline-flex justify-center items-center"
+                    >
+                      <span className="inline-block font-black">React </span>
+                    </motion.span>
+                  </p>
+                </motion.span>
+              </div>
+            )}
+            {slide === 2 && (
+              <div className="h-full flex flex-col justify-center">
+                <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw]">
+                  <span className="font-thin">Focused on </span>
+                  <span className="font-extralight">building </span>
+                </p>
+                <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw]">
+                  <span className="font-extralight">blazing-fast </span>
+                  <strong className="font-extrabold md:tracking-wider text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] scale-y-105">
+                    performant, scalable,
+                  </strong>
+                </p>
+                <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw]">
+                  <span className="font-thin">and </span>
+                  <strong className="font-extrabold md:tracking-wider text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] scale-y-105">
+                    seamlessly integrated
+                  </strong>
+                  <span className="font-extralight"> web apps </span>
+                </p>
+              </div>
+            )}
+            {slide === 3 && (
+              <div className="h-full flex flex-col justify-center">
+                <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw]">
+                  <span className="font-thin"> With </span>
+                  <span className="font-light">Code Speaks </span>
+                  <strong className="font-extrabold md:tracking-wider text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] scale-y-105">
+                    Performance
+                  </strong>
+                </p>
+                <p className="text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] md:leading-[5vw]">
+                  <span className="font-thing">I </span>
+                  <span className="font-extralight">value </span>
+                  <strong className="font-extrabold md:tracking-wider text-[2.3rem] sm:text-[3rem] md:text-[3.5vw] scale-y-105">
+                    Quality
+                  </strong>
+                  <span className="font-light"> over </span>
+                  <span className="font-thin opacity-90 text-[2.3rem] sm:text-[3rem] md:text-[3.5rem] text-neutral-300">
+                    Quantity
+                  </span>
+                </p>
+              </div>
+            )}
+          </SwiperSlide>
+        ))}
+        <div className="absolute [transition:opacity_0.3s] z-10 bottom-[7vh] top-[var(--swiper-pagination-top, auto)] left-[0] flex gap-[6vw] w-full">
+          {[1, 2, 3].map((slider) => {
+            const isActive = slider === activeSlide;
+            return (
+              <div
+                className={cn(
+                  "flex items-center gap-[2vw]",
+                  isActive ? "grow" : ""
+                )}
+                key={slider}
+              >
+                <span
+                  className={cn(
+                    "text-center text-[1.5rem] sm:text-[3rem] md:text-[1.1rem] text-foreground cursor-pointer",
+                    `font-${isActive ? "bold" : "thin"}`
+                  )}
+                >
+                  0{slider}.
+                </span>
+                {isActive && (
+                  <div className="h-[0.01rem] w-full bg-muted">
+                    <div
+                      className="h-full bg-foreground"
+                      style={{
+                        width: `${progress}%`,
+                        transitionTimingFunction: "ease-in-out",
+                      }}
+                    ></div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </Swiper>
+  )
+}
+export default memo(HeroSwiper) 
