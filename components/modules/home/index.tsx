2ff0aa35dfbcb672664bc8ff58986d0cdcd509ef
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-05-30 10:21:41 +0300

    enhance performance

--- components/modules/home/index.tsx
+++ components/modules/home/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const Hero = dynamic(() => import("@/components/modules/home/Hero"));
+// const Hero = dynamic(() => import("@/components/modules/home/Hero"));
 const About = dynamic(() => import("@/components/modules/home/About"));
 const SelectedWorks = dynamic(() => import("@/components/modules/home/Works"));
 const Expertise = dynamic(() => import("@/components/modules/home/Expertise"));
@@ -10,7 +10,7 @@ const Stack = dynamic(() => import("@/components/modules/home/Stack"));
 const Home = () => {
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
       <About />
       <SelectedWorks />
       <Expertise />
