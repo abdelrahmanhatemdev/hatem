970625e8a21d7dcd0719e08e7607d7a6add1eb4c
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-06-02 12:43:43 +0300

    fix linting errors

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,5 +1,4 @@
 import MainLayout from "@/components/custom/MainLayout";
-import { Metadata } from "next";
 import { ReactNode } from "react";
 
 export default function Home({children}: {children: ReactNode}) {
