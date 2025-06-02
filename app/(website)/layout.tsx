b44d9d17d43986ef6981adcc0e0d200e4475d726
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-06-02 11:58:51 +0300

    update metadata

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,17 +2,6 @@ import MainLayout from "@/components/custom/MainLayout";
 import { Metadata } from "next";
 import { ReactNode } from "react";
 
-export const metadata: Metadata = {
-  title: `Abdelrahman Hatem | Next.js Frontend Developer | Portfolio`,
-    description: "Experienced Next.js Frontend Developer with 6+ years in building scalable, high-performance web apps. Expert in React, TypeScript, and Tailwind CSS.",
-    icons:{
-      icon: "/favicon.ico", 
-      shortcut: "/favicon-32x32.png", 
-      apple: "/apple-touch-icon"
-    }, 
-    manifest: "/site.webmanifest"
-}
-
 export default function Home({children}: {children: ReactNode}) {
   return (
     <MainLayout>{children}</MainLayout>
